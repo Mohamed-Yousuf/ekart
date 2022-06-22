e30999d3ee7a6784b8fc1cc70a10753f396e4d1e
Mohamed-Yousuf <80694570+Mohamed-Yousuf@users.noreply.github.com>
2022-06-21 23:28:18 -1200

    Add files via upload

---
+++ products.js
@@ -0,0 +1,55 @@
+
+var products = {
+  123: {
+    name : "MokBook Thicc",
+    desc : "Greatest properly off ham exercise all.",
+    img : "mac.jpeg",
+    price : 2034
+  },
+  124: {
+    name : "Air Jordan",
+    img : "shoe.jpg",
+    price : 800
+  },
+  125: {
+    name : "Tshirt",
+    img : "tees.jpg",
+    price : 100
+  },
+  126: {
+    name : "sipper",
+    img : "sip.jpg",
+    price : 80
+  },
+  127: {
+    name : "Gym Bag",
+    img : "bag.jpg",
+    price : 84
+  },
+  128: {
+    name : "Mobile",  
+    img : "mob.jpg",
+    price : 500
+  },
+  129: {
+    name : "Cricket Bat",   
+    img : "bat.jpg",
+    price : 120
+  },
+  130: {
+    name : "Coolers",   
+    img : "glass.jpg",
+    price : 20
+  },
+  131: {
+    name : "fip-flop",
+    img : "flip.jpg",
+    price : 30
+  }, 132: {
+    name : "Watch",
+    img : "watch.jpg",
+    price : 70
+  },
+};
+
+  
